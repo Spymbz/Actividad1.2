d9cba47abb0e5ce1199dfdf9f4fb47782713fd4a
Spymb <benjasegovia.contacto@gmail.com>
2025-09-02 08:36:21 -0400

    commit de prueba

---
+++ app/src/main/java/com/example/github/Activity2.java
@@ -0,0 +1,40 @@
+package com.example.github;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Toast.makeText(this, "Datos recibidos \uD83D\uDC4D", Toast.LENGTH_SHORT).show();
+
+        TextView textView2 = findViewById(R.id.textView2);
+        String datoRecibido = getIntent().getStringExtra("datoEnviado");
+        textView2.setText("Hola " + datoRecibido + "!");
+
+    }
+    public void back(View view){
+        Intent volverMain = new Intent(this, MainActivity.class);
+        startActivity(volverMain);
+    }
+}
---
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.github;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void enviarDatos(View view){
+
+        EditText textbox = findViewById(R.id.textbox);
+        String nombre = textbox.getText().toString();
+
+        Intent Activity2 = new Intent(this, Activity2.class);
+        Activity2.putExtra("datoEnviado", nombre);
+        startActivity(Activity2);
+    }
+}
